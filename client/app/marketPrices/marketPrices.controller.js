3eaf267ad7e8d3cca96f9349c59864d25b4b669d
suceava <dan_suceava@yahoo.com>
2015-03-20 12:15:44 -0700

    format the grid columns

--- client/app/marketPrices/marketPrices.controller.js
+++ client/app/marketPrices/marketPrices.controller.js
@@ -17,18 +17,28 @@ angular.module('eliteApp')
 
     $scope.gridOptions = { 
       data: 'marketPrices',
-      groups: [ 'type' ],
+      groups: [ 'commodity.type' ],
       groupsCollapsedByDefault: false,
       enableSorting: false,
       multiSelect: false,
       columnDefs: [
         { field: 'commodity.name', displayName: 'COMMODITY' },
         { field: 'starport.name', displayName: 'STARPORT'},
-        { field: 'sellPrice', displayName: 'SELL' },
-        { field: 'buyPrice', displayName: 'BUY' },
-        { field: 'demand', displayName: 'DEMAND' },
-        { field: 'supply', displayName: 'SUPPLY' },
-        { field: 'priceDate', displayName: 'DATE', width: '200px' }
+        { field: 'sellPrice', displayName: 'SELL', width: '100px',
+          cellTemplate: '<div><div class="ngCellText">{{row.getProperty(col.field) | number}}</div></div>'
+        },
+        { field: 'buyPrice', displayName: 'BUY', width: '100px',
+          cellTemplate: '<div><div class="ngCellText">{{row.getProperty(col.field) | number}}</div></div>'
+        },
+        { field: 'demand', displayName: 'DEMAND', width: '200px',
+          cellTemplate: '<div><div class="ngCellText">{{row.getProperty(col.field) | number}} {{row.getProperty("demandString")}}</div></div>'
+        },
+        { field: 'supply', displayName: 'SUPPLY', width: '200px',
+          cellTemplate: '<div><div class="ngCellText">{{row.getProperty(col.field) | number}} {{row.getProperty("supplyString")}}</div></div>'
+        },
+        { field: 'priceDate', displayName: 'DATE', width: '200px',
+          cellTemplate: '<div><div class="ngCellText">{{row.getProperty(col.field) | date: "short"}}</div></div>'
+        }
       ],
       plugins: [ new ngGridFlexibleHeightPlugin({ maxHeight: $window.innerHeight - 350, window: $window }) ],
       
